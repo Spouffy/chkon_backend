d56fff1a1ca1ad2b1f7e5b511556f3e654bff0ab
Spouffy <65297560+Spouffy@users.noreply.github.com>
2023-02-04 13:06:54 +0100

    Init

---
+++ index.js
@@ -0,0 +1,28 @@
+const http = require('http')
+const express = require('express')
+const fs = require('fs')
+
+const port = process.env.PORT || 5400
+var app = express();
+
+// DATA collect
+var msg = require('./DATA/msg.json')
+
+// DATA API request handlers
+app.get('/api/msg', async (req, res, next) => {
+    var msg = require('./DATA/msg.json')
+	res.json(msg).status(200).end()
+})
+app.get('/api/indice', async (req, res, next) => {
+    var indice = require('./DATA/indice.json')
+    res.send(indice).status(200).end()
+})
+
+app.post('api/msg', async (req, res, next) => {
+
+    res.status(200).end()
+})
+
+// Server stuff
+var server = http.createServer(app);
+server.listen(port, () => console.log('Server running on port ' + port))
